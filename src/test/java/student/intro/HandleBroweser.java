074b415b4cda93bb04458686fdab493689fd12b5
alimu <alimuhamad.safa@gmail.com>
2024-07-09 21:19:15 -0400

    Handling Browser

---
+++ src/test/java/student/intro/HandleBroweser.java
@@ -0,0 +1,12 @@
+package student.intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleBroweser {
+    public static void main(String[] args) {
+        //Open Browser Chrome Browser
+        ChromeDriver chromeDriver = new ChromeDriver();
+        //Navigate to a URL
+        chromeDriver.get("https://google.com");
+    }
+}
