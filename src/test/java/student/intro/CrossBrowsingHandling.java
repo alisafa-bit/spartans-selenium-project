00a28fdeedbe5370cd51e383f6e4b6b13e4222ca
alimu <alimuhamad.safa@gmail.com>
2024-07-10 19:55:23 -0400

    opening with different browser

--- src/test/java/student/intro/CrossBrowsingHandling.java
+++ src/test/java/student/intro/CrossBrowsingHandling.java
@@ -1,20 +1,24 @@
 package student.intro;
 
 import org.openqa.selenium.WebDriver;
+import java.util.Scanner;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CrossBrowsingHandling {
     public static void main(String[] args) {
-        String browserType = "edge";
+
+        var scnObj = new Scanner(System.in);
+        System.out.print("Enter your browser please.");
+        String browserType = scnObj.next();
         WebDriver driver;
 
-        if(browserType.equals("Chrome")){
+        if(browserType.equalsIgnoreCase("Chrome")){
             driver = new ChromeDriver();
-        } else if (browserType.equals("firefox")) {
+        } else if (browserType.equalsIgnoreCase("firefox")) {//equalsIgnoreCase
            driver = new FirefoxDriver();
-        } else if (browserType.equals("edge")) {
+        } else if (browserType.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
         } else {
             throw new RuntimeException("wrong browser.");
