17b3e0dd429f0cc0161f133fbad1b2305dbbbad3
alimu <alimuhamad.safa@gmail.com>
2024-07-09 21:31:36 -0400

    Handling Browser

--- src/test/java/student/intro/NavigateToFacebook.java
+++ src/test/java/student/intro/NavigateToFacebook.java
@@ -5,7 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class NavigateToFacebook {
     public static void main(String[] args) {
         ChromeDriver enteringToFacebook = new ChromeDriver();
+
+        enteringToFacebook.manage().window().maximize();
+
+        // To open browser
         enteringToFacebook.get("https://www.facebook.com");
+        //To close browser entirely.
+        enteringToFacebook.quit();
 
     }
 }
