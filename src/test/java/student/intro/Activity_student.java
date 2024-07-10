58cb358346cf49571dfbe309a81b38cf2c709553
alimu <alimuhamad.safa@gmail.com>
2024-07-09 21:43:27 -0400

    getting the info

---
+++ src/test/java/student/intro/Activity_student.java
@@ -0,0 +1,13 @@
+package student.intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity_student {
+    public static void main(String[] args) {
+        ChromeDriver insuranceTitle = new ChromeDriver();
+        String title = insuranceTitle.getTitle();
+        insuranceTitle.manage().window().maximize();
+        System.out.println("https://dev.insurance.tekschool-students.com/");
+        insuranceTitle.quit();
+    }
+}
